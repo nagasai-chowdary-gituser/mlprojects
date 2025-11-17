d29fd147393956214f37fb8eba6553fb4058926b
nagasai chowdary <jonnalagaddanagasai6@gmail.com>
2025-11-17 16:38:09 +0530

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -5,7 +5,7 @@ def error_message_detail(error,error_detail:sys):
     filename=exc_tb.tb_frame.f_code.co_filename
     error_message="the file is [{0}],in the line [{1}],the error is [{2}]".format(filename,exc_tb.tb_lineno,str(error))
     return error_message
-class customexception(Exception):
+class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
@@ -16,4 +16,4 @@ if __name__=="__main__":
         a=1/0
     except Exception as e:
         logging.info("divide by zero")
-        raise customexception(e,sys)
+        raise CustomException(e,sys)
